889985618ae065b5fd798601a638b0260720d498
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-28 20:40:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeingOfArray(array, element) {
   var cities = ["seattle", "NY", "LA"];
   
-  
+  return cities.upshift
   
 
