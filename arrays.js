6c67ea13606982f35571ad441cf1dadbcc935ed8
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-28 20:48:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   var cities = ["seattle", "NY", "LA"];
   var addCity= "miami";
-  return cities.unshift(adCity);
+  return cities.unshift(addCity);
 }
   
 
