affd5c096952b7568da9240fdffa217e726bbd48
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-28 21:35:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningArray(array, element) {
   
  let newArray = array.unshift(element);
- return(array, newArray);
+ return(array);
 }
   
 
